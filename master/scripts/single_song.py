d2c618745e7589c9f7b246f9c629d0299364f78d
byebb <matthias.miller229@gmail.com>
2025-03-30 19:07:32 +0200

    update single song py file

--- master/scripts/single_song.py
+++ master/scripts/single_song.py
@@ -1,122 +1,126 @@
-# -- coding: utf-8 --
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Standard library imports
+import argparse
+import codecs
+import glob
+import io
+import json
+import os
 import sys
 import time
-import parseConfigFiles as Config
-import parseTextFiles as text
+import traceback
+import unicodedata
+
+# Third-party imports
+# (none in this file)
+
+# Local application imports
 import generateOutput as output
 import generateOutput7 as output7
-import os, codecs, io
-import glob
-import unicodedata
-import traceback
-import json
+import parseConfigFiles as Config
+import parseTextFiles as text
 
-import argparse
-
-# Check if interaction is needed
-parser = argparse.ArgumentParser(description='By default the interactive File Selector is opened for creation of single files...')   
-parser.add_argument("-t", "--type", help="Default: Interactive", default="interactive")
-parser.add_argument("-l", "--limit", help="Default: ALL", default="0")
-parser.add_argument("-s", "--style", help="Default: CONSOLE", default="CONSOLE")
-parser.add_argument("-n", "--song", help="Default: NONE", default="none")
-args = parser.parse_args()
-
-print_style="CONSOLE"
-if args.style is not "CONSOLE":
-    print_style="WEBSITE"
-
-
-def PrintC(style, content):
-    if (style is "CONSOLE"):
+def print_formatted(style, content):
+    """Print content based on output style (console or website)"""
+    if style == "CONSOLE":
         print(content)
     else:
         print(content + "<br>")
 
-# Create an empty dictionary to store all the parsed configuration data
-configAll = {}
-try:
+def main():
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description='By default the interactive File Selector is opened for creation of single files...')   
+    parser.add_argument("-t", "--type", help="Default: Interactive", default="interactive")
+    parser.add_argument("-l", "--limit", help="Default: ALL", default="0")
+    parser.add_argument("-s", "--style", help="Default: CONSOLE", default="CONSOLE")
+    parser.add_argument("-n", "--song", help="Default: NONE", default="none")
+    args = parser.parse_args()
 
-    # Get the directory path for the folder containing the text files
-    # loop over all files in master
-    __file = __file__.decode(sys.getfilesystemencoding())
-    dirname = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file)), os.pardir))
-    folder = u"server_uploads"
+    print_style = "CONSOLE"
+    if args.style != "CONSOLE":
+        print_style = "WEBSITE"
 
-    root = os.path.join(dirname, folder)   
-
-    filename = ""
-
-    if args.song is not "none":
-        filename = args.song
-                            
-    songs_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file)), os.pardir))
-    songs_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(songs_path)), os.pardir))
-    songs_path = os.path.abspath(os.path.dirname(os.path.abspath(songs_path)))
-        
-    filename = str(root) + "/" + str(filename)
-
-    base = os.path.basename(filename)
-    base = base[:-4]
-
-    # Parse all configuration files and add their data to configAll
-    Config.ParseConfigFiles(configAll, print_style, base.replace(" ", "_"))
-
-    PrintC(print_style, '============================================')
-    PrintC(print_style, u'[START] CONVERTING SELECTED SONGS...')
-    PrintC(print_style, u'[FOLDER] '+root+'...')
-    PrintC(print_style, '============================================')
-
-    # Start Library Creation Timer
-    startLibraryTimer = time.time()
-
-    completed_songs = []
-
+    # Create an empty dictionary to store all the parsed configuration data
+    config_all = {}
     try:
-        # Calculate performance of every song
-        start_song_timer = time.time()
+        # Get the directory path for the folder containing the text files
+        # loop over all files in master
+        __file_decoded = __file__.decode(sys.getfilesystemencoding())
+        dirname = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file_decoded)), os.pardir))
+        folder = u"server_uploads"
 
-        # If filepath is not unicode, convert it to unicode using utf-8 encoding
-        if not isinstance(filename, unicode):
-            filename = unicode(filename, "utf-8")
+        root = os.path.join(dirname, folder)   
 
-        # Get the base name of the file
-        filename = os.path.basename(filename)
+        filename = ""
 
-        # If the file is a text file, generate output files for it
-        if filename.endswith(".txt"):
-            # print('-----------------------------')
-            # print(u'[START] "{0}"'.format(filepath[:-4]))
+        if args.song != "none":
+            filename = args.song
+                                
+        songs_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file_decoded)), os.pardir))
+        songs_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(songs_path)), os.pardir))
+        songs_path = os.path.abspath(os.path.dirname(os.path.abspath(songs_path)))
+            
+        filename = str(root) + "/" + str(filename)
 
-            # Parse the text file and get its data
-            inputText = text.ParseTextFile(root, filename, configAll["groupConfigs"])
+        base = os.path.basename(filename)
+        base = base[:-4]
 
-            # Loop over each configuration and generate output files
-            for Config in configAll["fileConfigs"]:
-                output.CreateOutputs(Config, configAll["groupConfigs"], inputText)
+        # Parse all configuration files and add their data to config_all
+        Config.ParseConfigFiles(config_all, print_style, base.replace(" ", "_"))
 
-            # Loop over each configuration from pro7 and generate output files
-            for Config in configAll["fileConfigs7"]:
-                output7.CreateOutputs(Config, configAll["groupConfigs7"], inputText)
+        print_formatted(print_style, '============================================')
+        print_formatted(print_style, u'[START] CONVERTING SELECTED SONGS...')
+        print_formatted(print_style, u'[FOLDER] '+root+'...')
+        print_formatted(print_style, '============================================')
 
-            executionTime = (time.time() - start_song_timer)
-            PrintC(print_style, u'[RUNTIME] [ {1} sec ] \t [ {0} ]'.format(filename[:-4], str(round(executionTime, 2))))           
+        # Start Library Creation Timer
+        start_library_timer = time.time()
 
-    # If an error occurs, print the error message
-    # except ValueError as err:
-        # PrintC(print_style, err)
-    except:
-        traceback.print_exc()
+        completed_songs = []
 
-    executionTime = (time.time() - startLibraryTimer)
-    PrintC(print_style, '============================================')
-    PrintC(print_style, u'[TOTAL RUNTIME] {0} seconds (all selected files)'.format(str(round(executionTime, 1))))
-    PrintC(print_style, "Generation Successful. This window can be closed")
-    PrintC(print_style, '============================================')
+        try:
+            # Calculate performance of every song
+            start_song_timer = time.time()
 
-# If an error occurs during configuration file parsing, print the error message
-except ValueError as err:
-    print("Exception occurred: check following text:")
-    print(err)
+            # If filepath is not unicode, convert it to unicode using utf-8 encoding
+            if not isinstance(filename, unicode):
+                filename = unicode(filename, "utf-8")
+
+            # Get the base name of the file
+            filename = os.path.basename(filename)
+
+            # If the file is a text file, generate output files for it
+            if filename.endswith(".txt"):
+                # Parse the text file and get its data
+                input_text = text.ParseTextFile(root, filename, config_all["groupConfigs"])
+
+                # Loop over each configuration and generate output files
+                for config in config_all["fileConfigs"]:
+                    output.CreateOutputs(config, config_all["groupConfigs"], input_text)
+
+                # Loop over each configuration from pro7 and generate output files
+                for config in config_all["fileConfigs7"]:
+                    output7.CreateOutputs(config, config_all["groupConfigs7"], input_text)
+
+                execution_time = (time.time() - start_song_timer)
+                print_formatted(print_style, u'[RUNTIME] [ {1} sec ] \t [ {0} ]'.format(filename[:-4], str(round(execution_time, 2))))           
+
+        except Exception:
+            traceback.print_exc()
+
+        total_execution_time = (time.time() - start_library_timer)
+        print_formatted(print_style, '============================================')
+        print_formatted(print_style, u'[TOTAL RUNTIME] {0} seconds (all selected files)'.format(str(round(total_execution_time, 1))))
+        print_formatted(print_style, "Generation Successful. This window can be closed")
+        print_formatted(print_style, '============================================')
+
+    # If an error occurs during configuration file parsing, print the error message
+    except ValueError as err:
+        print("Exception occurred: check following text:")
+        print(err)
+
+if __name__ == "__main__":
+    main()
 
